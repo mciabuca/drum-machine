a3ec33c5c25a08a1b81a3ded2ed8c6143c8c09ea
Michael Ciabuca <other404@pm.me>
2024-01-11 16:19:39 +0200

    moved audio files and changed imports for gh-pages

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import DrumPad from './DrumPad'; 
 import './DrumMachine.css';
+import Heater1 from './audio/Heater-1.mp3';
+import Heater2 from './audio/Heater-2.mp3';
+import Heater3 from './audio/Heater-3.mp3';
+import Heater4 from './audio/Heater-4_1.mp3';
+import Heater6 from './audio/Heater-6.mp3';
+import CevH2 from './audio/Cev_H2.mp3';
+import DscOh from './audio/Dsc_Oh.mp3';
+import KicknHat from './audio/Kick_n_Hat.mp3';
+import RP4KICK1 from './audio/RP4_KICK_1.mp3';
 
 const drumPads = [
-  { id: 'Heater-1', letter: 'Q', src: '/audio/Heater-1.mp3' },
-  { id: 'Heater-2', letter: 'W', src: '/audio/Heater-2.mp3' },
-  { id: 'Heater-3', letter: 'E', src: '/audio/Heater-3.mp3' },
-  { id: 'Heater-4', letter: 'A', src: '/audio/Heater-4_1.mp3' },
-  { id: 'Heater-6', letter: 'S', src: '/audio/Heater-6.mp3' },
-  { id: 'Cev', letter: 'D', src: '/audio/Cev_H2.mp3' },
-  { id: 'Disc', letter: 'Z', src: '/audio/Dsc_Oh.mp3' },
-  { id: 'Kick & Hat', letter: 'X', src: '/audio/Kick_n_Hat.mp3' },
-  { id: 'Kick', letter: 'C', src: '/audio/RP4_KICK_1.mp3' },
+  { id: 'Heater-1', letter: 'Q', src: Heater1 },
+  { id: 'Heater-2', letter: 'W', src: Heater2 },
+  { id: 'Heater-3', letter: 'E', src: Heater3 },
+  { id: 'Heater-4', letter: 'A', src: Heater4 },
+  { id: 'Heater-6', letter: 'S', src: Heater6 },
+  { id: 'Cev', letter: 'D', src: CevH2 },
+  { id: 'Disc', letter: 'Z', src: DscOh },
+  { id: 'Kick & Hat', letter: 'X', src: KicknHat },
+  { id: 'Kick', letter: 'C', src: RP4KICK1 },
 ];
 
 class DrumMachine extends React.Component {
